49bc59688f3a2c4ae5b31ebb2449a25215de28d1
jpradas1 <jpradas@unal.edu.co>
2023-03-17 10:44:50 +0100

    first commit

---
+++ ETL.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import re
+import os
+
+class ETL_class(object):
+
+    def __init__(self, path_titles: str, path_rating: str):
+        self.path_title = path_titles
+        self.path_rating = path_rating
+
+    def get_csv_files(self, path):
+        raw_files = os.listdir(path)
+        file = []
+        for item in raw_files:
+            p = re.findall("(\w*\.csv)", item)
+            if p:
+                file.append(p[0])
+        return file
+
+    def etl_movies(self, platform: str):
+        df = pd.read_csv(self.path_title + platform)
+        
+        # we create an id for each platform's movie
+        df['id'] = str(platform[0]) + df['show_id']
+
+        # fill nan values by G, which depicts “general for all audiences”
+        df['rating'].fillna('G', inplace=True)
+
+        # transform datae_added column to YY-MM-DD date format
+        df['date_added'] = pd.to_datetime(df['date_added'])
+
+        # come every register into lowercases
+        df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+
+        # here we create two new columns to split duration data into numerical and type data
+        df['duration_int'] = df[['duration']].replace('\D*', '', regex=True).astype(float)
+        df['duration_type'] = df[['duration']].replace('\d*\s', '', regex=True)
+
+        # let's normilize data in duration_type columns
+        df['duration_type'].replace('seasons', 'season', inplace=True)
+
+        columns = ['id', 'show_id', 'type', 'title', 'director', 'cast', 'country', 
+                'date_added', 'release_year', 'rating', 'duration', 'duration_int',
+                'duration_type', 'listed_in', 'description']
+        df = df.reindex(columns=columns)
+        return df
+
+    def get_movies(self):
+        df_titles = []
+        titles = self.get_csv_files(self.path_title)
+        for platform in titles:
+            df_titles.append(self.etl_movies(platform))
+            
+        df_titles = pd.concat(df_titles)
+        return df_titles
+
+    def get_ratings(self):
+        df_rating = []
+        rating = np.sort(self.get_csv_files(self.path_rating))
+        for dc in rating:
+            df_rating.append(pd.read_csv(self.path_rating + dc))
+
+        df_rating = pd.concat(df_rating)
+        df_rating.drop_duplicates(inplace=True)
+        return df_rating
---
+++ main.py
@@ -0,0 +1,89 @@
+from fastapi import FastAPI
+# from pydantic import BaseModel
+# from typing import Optional
+
+import numpy as np
+import pandas as pd
+from unidecode import unidecode
+
+# import sys
+# sys.path.append('../')
+
+from ETL import ETL_class
+
+app = FastAPI()
+
+path_titles = './dataset/titles/'
+path_rating = './dataset/ratings/'
+etl = ETL_class(path_titles, path_rating)
+
+df_movies = etl.get_movies()
+df_ratings = etl.get_ratings()
+
+# last_year = np.sort(df_movies['release_year'].unique())[-1]
+
+@app.get("/function1/{year},{platform},{duration_type}")
+def get_max_duration(year: int, platform: str, duration_type: str):
+    duration_type = unidecode(duration_type).lower()
+    platform = unidecode(platform).lower()
+    
+    df = df_movies[['id','release_year','duration_int','duration_type']]
+
+    begins = platform[0]
+    df = df.loc[(df['duration_type'] == duration_type) & (df['release_year'] == year)]
+    df = df.loc[df['id'].str.contains('^{}'.format(begins))]
+
+    idx = df.loc[df.duration_int == df.duration_int.max()].id.values[0]
+
+    row = df_movies.loc[df_movies['id'] == str(idx)][['id','title', 'duration_int', 'duration_type']]
+    column = row.columns
+
+    result = {c: v for (c,v) in zip(column, row.values[0])}
+    # print("The longest movie or series in duration [{}] is '{}', it lasts {} {}".format(duration_type, row['title'].values[0],row['duration_int'].values[0],duration_type))
+    return result
+
+@app.get("/function2/{platform},{scored},{year}")
+def get_score_count(platform: str, scored: float, year: int):
+    platform = unidecode(platform).lower()
+    
+    begins = platform[0]
+    df = df_movies[['id', 'release_year']]
+    df = df.loc[df.release_year == year]
+    df = df.loc[df['id'].str.contains('^{}'.format(begins))]
+    
+    merge = df.merge(df_ratings[['movieId', 'rating']], left_on='id',right_on='movieId')
+    merge = merge[['id', 'rating']].groupby(['id']).agg('mean')
+
+    result = merge[merge.rating >= scored].shape[0]
+    # return result
+    return 'result: {}'.format(result)
+
+@app.get("/function3/{platform}")
+def get_count_platform(platform: str):
+    # if platform == None:
+    #     result = df_movies[['id']].drop_duplicates()
+    #     result = df_movies.shape[0]
+    #     return result
+    # else:
+    platform = unidecode(platform).lower()
+    begins = platform[0]
+    df = df_movies.loc[df_movies['id'].str.contains('^{}'.format(begins))]
+    result = df.shape[0]
+    return result
+
+@app.get("/function4/{platform},{year}")
+def get_actor(platform: str, year: int):
+    begins = platform[0]
+    df = df_movies[['id','cast', 'release_year']].loc[df_movies.release_year == year]
+    df = df.loc[df.id.str.contains('^{}'.format(begins))]
+    
+    actors = [a for x in df.cast.values for a in str(x).strip( ).split(', ')]
+    actors = [unidecode(x).lower() for x in actors]
+    
+    values, counts = np.unique(actors, return_counts=True)
+    cast = {v: int(c) for v, c in zip(values,counts)}
+    
+    sorted_cast = dict(sorted(cast.items(), key=lambda item: item[1], reverse=True))
+    max_keys = {k: v for k, v in list(sorted_cast.items())[:5]}
+    
+    return max_keys
